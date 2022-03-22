e95d78cf235d3b6fa45b8e034d6f41d2bf91bb61
jose vazquez <vazquez.jm@live.com>
2022-03-22 19:45:26 +0100

    Tema 3

--- tema0/tema0.py
+++ tema0/tema0.py
@@ -1,3 +1,18 @@
 
 nombre="josito"
 print(f"- Hola, soy {nombre} \n -Estoy empezando el curso de python \n- Espero aprender mucho")
+print(type(nombre))
+nombre="luisito"
+print(f"- Hola, soy {nombre} \n -Estoy empezando el curso de python \n- Espero aprender mucho")
+print(type(nombre))
+#Tema 3
+nombre="jose"
+apellidos="vazquez"
+edad=55
+email="vazquez.jm@live.com"
+telefono="616132794"
+casado=True
+hijos=True
+listAmigos=["Carlos", "Juan", "tono"]
+peliculasVistas={"p1":"termintar","p2":"el muro"}
+print(nombre,apellidos,email,telefono,casado,hijos,listAmigos,peliculasVistas)
